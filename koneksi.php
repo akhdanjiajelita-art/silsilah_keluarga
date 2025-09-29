1281488ea7adf231dc3c877e61f91c7fd510cc34
akhdanjiajelita <akhdanjiajelita@gmail.com>
2025-09-29 18:37:51 +0700

    ganti koneksi

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "sql313.infinityfree.com";
 $user = "if0_39914601";
 $pass = "P9B1jGZPNimbS7";
-$db   = "if0_39914601_XXX";
+$db   = "if0_39914601_db_keluarga";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
